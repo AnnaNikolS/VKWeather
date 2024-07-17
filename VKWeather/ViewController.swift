021f9f070119a6329f36cc580969c0c1cde499e9
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-07-17 21:56:55 +0300

    Initial Commit

---
+++ VKWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKWeather
+//
+//  Created by Анна on 17.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
